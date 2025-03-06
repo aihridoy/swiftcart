8ea5f04ddf2384bb0e234745cffdb2c6952ac46e
aihridoy <cse_1912020168@lus.ac.bd>
2025-03-07 03:37:32 +0600

    init: wishlist component

--- app/page.js
+++ app/page.js
@@ -1,15 +1,11 @@
-import Account from "@/components/Account";
 import Ads from "@/components/Ads";
 import Banner from "@/components/Banner";
-import Checkout from "@/components/Checkout";
 import CopyRight from "@/components/CopyRight";
 import Features from "@/components/Features";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
 import NewArrival from "@/components/NewArrival";
-import ProductDetails from "@/components/ProductDetails";
-import Shop from "@/components/Shop";
 import ShopByCategory from "@/components/ShopByCategory";
 import Trending from "@/components/Trending";
 
@@ -26,10 +22,6 @@ export default function Home() {
       <Trending />
       <Footer />
       <CopyRight />
-      {/* <ProductDetails /> */}
-      {/* <Account /> */}
-      {/* <Checkout /> */}
-      <Shop />
     </>
   );
 }
