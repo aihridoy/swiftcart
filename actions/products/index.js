94ef3213af5d691f218664522e33fbb704ebf7ae
aihridoy <cse_1912020168@lus.ac.bd>
2025-03-20 02:12:04 +0600

    feat: get all products

--- actions/products/index.js
+++ actions/products/index.js
@@ -1,7 +1,13 @@
-//add new product
 import api from "@/lib/axios";
 
+//add new product
 export const addProduct = async (productData) => {
-  const response = await api.post("/products", productData);
+  const response = await api.post("/add-product", productData);
   return response.data;
 };
+
+//get all products
+export const getProducts = async () => {
+    const response = await api.get("/products");
+    return response.data;
+}
--- app/api/products/route.js
+++ app/api/products/route.js
@@ -1,40 +1,14 @@
-import { NextResponse } from "next/server";
-import { dbConnect } from "@/service/mongo";
 import { Product } from "@/models/product-model";
+import { dbConnect } from "@/service/mongo";
+import { NextResponse } from "next/server";
 
-export async function POST(req) {
-  await dbConnect();
+export async function GET() {
+    await dbConnect();
 
-  try {
-    const body = await req.json(); 
-
-    const { title, brand, category, sku, price, description, mainImage, quantity } = body;
-    if (!title || !brand || !category || !sku || !price || !description || !mainImage || !quantity) {
-      return NextResponse.json({ error: "All required fields must be filled" }, { status: 400 });
-    }
-
-    const product = new Product({
-      title,
-      availability: body.availability || "In Stock",
-      brand,
-      category,
-      sku,
-      price: parseFloat(price),
-      originalPrice: body.originalPrice ? parseFloat(body.originalPrice) : null,
-      description,
-      quantity: parseInt(quantity, 10),
-      mainImage,
-      thumbnails: body.thumbnails || [],
-    });
-
-    await product.save();
-
-    return NextResponse.json({ message: "Product added successfully", product }, { status: 201 });
-  } catch (error) {
-    console.error("Error adding product:", error);
-    if (error.code === 11000) {
-      return NextResponse.json({ error: "SKU must be unique" }, { status: 400 });
-    }
-    return NextResponse.json({ error: "Failed to add product" }, { status: 500 });
-  }
+    try {
+        const products = await Product.find().lean(); // find all products and converts to plain object
+        return NextResponse.json({ products }, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ error: error.message }, { status: 500 });
+    }   
 }
