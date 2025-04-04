a31b038614a202cb2da8deac900f3a8e3c5c8b1c
aihridoy <cse_1912020168@lus.ac.bd>
2025-04-04 23:16:30 +0600

    fix: transform fetch to axios

--- actions/contact/index.js
+++ actions/contact/index.js
@@ -1,19 +1,11 @@
+import api from "@/lib/axios";
+
 export async function sendEmail({ to, subject, html }) {
     try {
-        const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/send-email`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ to, subject, html }),
-        });
-
-        const result = await response.json();
-        if (!response.ok) throw new Error(result.error || "Failed to send email");
-
-        return result;
+      const response = await api.post(`/send-email`, { to, subject, html });
+      return response.data; 
     } catch (error) {
-        console.error("Error in sendEmail action:", error);
-        throw error;
+      console.error("Error in sendEmail action:", error);
+      throw error; 
     }
-};
+  }
