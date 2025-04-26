50bbdf36be3a45f9d3b95cc1b833f38a77bc6999
aihridoy <cse_1912020168@lus.ac.bd>
2025-04-26 21:42:31 +0600

    fix: middleware issue

--- middleware.js
+++ middleware.js
@@ -1,48 +1,48 @@
-// import { NextResponse } from "next/server";
-// import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
 
-// const protectedRoutes = [
-//   "/wishlist",
-//   "/cart",
-//   "/orders",
-//   "/dashboard",
-//   "/dashboard/add-product",
-//   "/dashboard/users",
-//   "/dashboard/orders",
-//   "/dashboard/products-list",
-//   "/products/[id]",
-//   "/edit-product/[id]",
-// ];
+const protectedRoutes = [
+  "/wishlist",
+  "/cart",
+  "/orders",
+  "/dashboard",
+  "/dashboard/add-product",
+  "/dashboard/users",
+  "/dashboard/orders",
+  "/dashboard/products-list",
+  "/products/[id]",
+  "/edit-product/[id]",
+];
 
-// export async function middleware(request) {
-//   const { pathname } = request.nextUrl;
+export async function middleware(request) {
+  const { pathname } = request.nextUrl;
 
-//   const isProtectedRoute = protectedRoutes.some(
-//     (route) => pathname === route || pathname.startsWith(`${route}/`)
-//   );
+  const isProtectedRoute = protectedRoutes.some(
+    (route) => pathname === route || pathname.startsWith(`${route}/`)
+  );
 
-//   const token = await getToken({
-//     req: request,
-//     secret: process.env.AUTH_SECRET,
-//   });
+  const token = await getToken({
+    req: request,
+    secret: process.env.AUTH_SECRET,
+  });
 
-//   if (!token && isProtectedRoute) {
-//     const url = new URL("/login", request.url);
-//     url.searchParams.set("callbackUrl", pathname);
-//     return NextResponse.redirect(url);
-//   }
+  if (!token && isProtectedRoute) {
+    const url = new URL("/login", request.url);
+    url.searchParams.set("callbackUrl", pathname);
+    return NextResponse.redirect(url);
+  }
 
-//   return NextResponse.next();
-// }
+  return NextResponse.next();
+}
 
-// export const config = {
-//   matcher: [
-//     /*
-//      * Match all request paths except for:
-//      * 1. /_next (Next.js internals)
-//      * 2. /static (static files)
-//      * 3. /favicon.ico, /robots.txt (common files)
-//      */
-//     "/((?!_next|static|favicon.ico|robots.txt).*)",
-//   ],
-// };
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for:
+     * 1. /_next (Next.js internals)
+     * 2. /static (static files)
+     * 3. /favicon.ico, /robots.txt (common files)
+     */
+    "/((?!_next|static|favicon.ico|robots.txt).*)",
+  ],
+};
